fcb1e16eaccd95bc10826b8ec3bf20437df036ba
pratimachinthala <pratima.chinthala@gmail.com>
2018-03-13 22:06:12 -0700

    Intial Commit

---
+++ eclipse-workspace/SampleJava/src/Sample.java
@@ -0,0 +1,17 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My sample project to test");
+		System.out.println("My sample project to test");
+
+		System.out.println("My sample project to test");
+		System.out.println("My sample project to test");
+		System.out.println("My sample project to test");
+
+
+
+	}
+
+}
